c1f717491eab98859db9759c9d9f83ff391bed7f
alexve10 <40767818+alexve10@users.noreply.github.com>
2018-07-08 16:40:34 -0500

    Add files via upload

---
+++ 1era Parte.py
@@ -0,0 +1,6 @@
+brazos=["triceps","biceps","planchas","barras","levantar pesas"]
+piernas=["pantorrillas", "muslos", "sentadillas","polichinelas"]
+torso=["planchas","abdominales","pectorales","salto de tijera", "face pull"]
+cardio=["correr","trotar","nadar","saltar soga","burpee", "aerobicos"]
+dieta_sobrepeso=["Desayuno: Leche deslactosada, jugo de naranja y 2 tostadas de pan integral con queso fresco","Almuerzo: 1 o 2 piezas de fruta con yogurt desnatado", "Cena: Verdura cocida al horno o a la plancha"]
+dieta_obesidad=["Desayuno: Café o infusión, leche desnatada y pan con queso desnatado","Almuerzo: Ensalada de apio, nueces y uvas o pescado al horno con tomate","Cena: Ensalada de verduras"]
